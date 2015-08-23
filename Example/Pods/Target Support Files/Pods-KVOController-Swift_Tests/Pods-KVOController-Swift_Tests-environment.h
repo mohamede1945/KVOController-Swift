1bfaaa1a5d143f9ff8fa1c91fc50f87e82026a71
Mohamed Afifi <mohamede1945@gmail.com>
2015-08-23 10:02:45 +0200

    Updated travis and pod specs.

--- Example/Pods/Target Support Files/Pods-KVOController-Swift_Tests/Pods-KVOController-Swift_Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-KVOController-Swift_Tests/Pods-KVOController-Swift_Tests-environment.h
@@ -8,7 +8,7 @@
 
 // KVOController-Swift
 #define COCOAPODS_POD_AVAILABLE_KVOController_Swift
-#define COCOAPODS_VERSION_MAJOR_KVOController_Swift 0
-#define COCOAPODS_VERSION_MINOR_KVOController_Swift 1
+#define COCOAPODS_VERSION_MAJOR_KVOController_Swift 1
+#define COCOAPODS_VERSION_MINOR_KVOController_Swift 0
 #define COCOAPODS_VERSION_PATCH_KVOController_Swift 0
 
